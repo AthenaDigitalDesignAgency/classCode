3d55acce999bc8e5cd6cbabd92dedc74447b934f
awdriggs <awdriggs@gmail.com>
2016-03-15 20:04:59 +0000

    do now done

---
+++ forLoops/lesson2/doNow/solution.js
@@ -0,0 +1,10 @@
+//alert('js working');
+
+for(var i = 0; i < 10; i++){
+    //run this code
+    document.write('Taylor Swift');
+}
+
+//write a for loop
+//repeat 20 times
+//print "hello" to screen
